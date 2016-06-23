5febcced35ec8c9a4ef51870371a93e81ccec01e
MWI-Tobias <alexandra.hunziger@gmail.com>
2016-06-23 11:16:03 +0200

    Projekt fast fertiggestellt

--- app/src/main/java/de/tofe/cofeto/MainActivity.java
+++ app/src/main/java/de/tofe/cofeto/MainActivity.java
@@ -1,47 +1,179 @@
 package de.tofe.cofeto;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import android.app.Activity;
-import android.app.ProgressDialog;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.nio.channels.Channel;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestFactory;
+import com.google.api.client.http.HttpResponse;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
 
-import de.tofe.cofeto.service.CountriesServiceCallback;
-import de.tofe.cofeto.service.RestCountriesService;
+public class MainActivity extends Activity {
 
-public class MainActivity extends Activity implements CountriesServiceCallback {
+    public static final String TAG4LOGGING = "Cofeto";
 
-    private TextView countriesTextView;
+    //Button mit dem der Web-Request gestartet wird
+    protected Button _suchButton = null;
 
-    private RestCountriesService service;
+    //Eingabefeld für die Suche
+    protected EditText _landEditText = null;
 
-    private ProgressDialog dialog;
+    //Eingabe-String
+    protected String _eingabeLand = "";
 
+    //TextView zur Anzeige des Ergebnisses des Web-Requests auch zur Anzeige von Fehlermeldungen
+    protected TextView _ergebnisTextView = null;
+
+
+    //Lifecycle-Methode; Layout für UI laden und Referenzen auf UI-Elemente holen
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        countriesTextView = (TextView)findViewById(R.id.ergebnisFeld);
+        _suchButton       = (Button)   findViewById( R.id.suchButton );
+        _landEditText     = (EditText) findViewById( R.id.landEditText );
+        _ergebnisTextView = (TextView) findViewById( R.id.ergebnisTextView );
 
-        service = new RestCountriesService(this);
-        dialog = new ProgressDialog(this);
-        dialog.setMessage("Laden...");
-        dialog.show();
-
+        _ergebnisTextView.setMovementMethod(new ScrollingMovementMethod()); // um vertikales Scrolling zu ermöglichen
     }
 
-    @Override
-    public void serviceSuccess(Channel channel) {
-        dialog.hide();
+    //Tastatur nach Suche ausblenden
+    /*InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+    mgr.hideSoftInputFromWindow(curEditText.getWindowToken(), 0);
+    mgr.hideSoftInputFromWindow(this.getCurrentFocus().getWindowToken(),InputMethodManager.HIDE_NOT_ALWAYS);*/
+
+    //Listener für Tastatur-Suchbutton
+    /*_landEditText.setOnEditorActionListener(new OnEditorActionListener() {
+        @Override
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+            boolean handled = false;
+            if (actionId == EditorInfo.IME_ACTION_SEND) {
+                sendMessage();
+                handled = true;
+            }
+            return handled;
+        }
+    });*/
+
+    //Event-Handler für Start-Button, wird in Layout-Daten mit Attribut "android:onClick" zugewiesen
+    public void onStartButtonBetaetigt(View view) {
+
+        _suchButton.setEnabled(false); // Button deaktivieren während ein HTTP-Request läuft
+
+        _eingabeLand = _landEditText.getText().toString();
+
+        // Hintergrund-Thread mit HTTP-Request starten
+        MeinHintergrundThread mht = new MeinHintergrundThread();
+        mht.start();
     }
 
-    @Override
-    public void serviceFailure(Exception exception) {
-        dialog.hide();
-        Toast.makeText(this, exception.getMessage(), Toast.LENGTH_LONG).show();
+
+    //In dieser Methode wird der HTTP-Request zur Web-API durchgeführt
+    protected String holeDatenVonWebAPI() throws Exception {
+
+        //1: Request-Factory holen
+        HttpTransport      httpTransport  = new NetHttpTransport();
+        HttpRequestFactory requestFactory = httpTransport.createRequestFactory();
+
+        //2: URL erzeugen und ggf. URL-Parametern hinzufügen
+        GenericUrl url = new GenericUrl("https://restcountries.eu/rest/v1/name/" + _eingabeLand + "?fullText=true");
+
+        //3: eigentliches Absetzen des Requests
+        HttpRequest  request      = requestFactory.buildGetRequest(url);
+        HttpResponse httpResponse = request.execute();
+
+        //4: Antwort-String (JSON-Format) zurückgeben
+        String jsonResponseString = httpResponse.parseAsString();
+
+        Log.i(TAG4LOGGING, "JSON-String erhalten: " + jsonResponseString);
+
+        return jsonResponseString;
     }
+
+
+    //Parsen des JSON-Dokuments <i>jsonString</i>, das von der Web-API zurückgeliefert wurde
+    protected String parseJSON(String jsonString) throws Exception {
+
+        if (jsonString == null || jsonString.trim().length() == 0) {
+            return "Keine Informationen zu dem Suchbegriff gefunden.";
+        }
+
+        JSONArray jsonArray = new JSONArray(jsonString); // eigentliches Parsen
+
+        String capital = "";
+        String subregion = "";
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject jsonobject = jsonArray.getJSONObject(i);
+            capital = jsonobject.getString("capital");
+            subregion = jsonobject.getString("subregion");
+        }
+
+        // String für Ausgabe auf UI zusammenbauen
+        return "Hauptstadt: " + capital  + "\nRegion: " + subregion;
+    }
+
+
+	/* *************************** */
+	/* *** Start innere Klasse *** */
+	/* *************************** */
+
+    // Zugriff auf Web-API (Internet-Zugriff) wird in eigenen Thread ausgelagert
+    protected class MeinHintergrundThread extends Thread {
+
+        //Der Inhalt in der überschriebenen <i>run()</i>-Methode wird in einem Hintergrund-Thread ausgeführt
+        @Override
+        public void run() {
+
+            try {
+                String jsonDocument = holeDatenVonWebAPI();
+
+                String ergString = parseJSON(jsonDocument);
+
+                ergbnisDarstellen( ergString );
+            }
+            catch (Exception ex) {
+                //IN Toast auslagern, ODER nicht anzeigen
+                ergbnisDarstellen( "Exception aufgetreten: " + ex.getMessage() );
+            }
+        }
+
+
+        //Methode um Ergebnis-String in TextView darzustellen
+        protected void ergbnisDarstellen(String ergebnisStr) {
+
+            final String finalString = ergebnisStr;
+
+            _suchButton.post( new Runnable() { // wir könnten auch die post()-Methode des TextView-Elements verwenden
+                @Override
+                public void run() {
+
+                    _suchButton.setEnabled(true);
+
+                    _ergebnisTextView.setText(finalString);
+                }
+            });
+
+        }
+
+    };
+
+	/* *************************** */
+	/* *** Ende innere Klasse  *** */
+	/* *************************** */
+
 }
